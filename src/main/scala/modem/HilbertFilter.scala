2a5d96b515266373028b29fe7679c3e674da95be
Griffin Prechter <griffinprechter@MacBook-Pro-2.local>
2021-03-13 17:36:31 -0800

    using wider bits

--- src/main/scala/modem/HilbertFilter.scala
+++ src/main/scala/modem/HilbertFilter.scala
@@ -30,10 +30,10 @@ class HilbertFilter(params: BLEBasebandModemParams) extends Module {
   val I_scaled = Wire(SInt((params.adcBits + 1).W))
   val Q_scaled = Wire(SInt((params.adcBits + 1).W))
 
-  var coeffs = Seq[String]("b000000000000", "b000000000000", "b000000000000", "b000000000100", "b000000000000", "b000000010000", "b000000000000", "b000000110100", "b000000000000", "b000010001011", "b000000000000", "b000101011100", "b000000000000", "b010011111000", "b000000000000", "b101100001000", "b000000000000", "b111010100100", "b000000000000", "b111101110101", "b000000000000", "b111111001100", "b000000000000", "b111111110000", "b000000000000", "b111111111100", "b000000000000", "b000000000000", "b000000000000").map(c => c.U.asFixedPoint(11.BP))
+  var coeffs = Seq[String]("b000000000000", "b000000000000", "b000000000000", "b000000000100", "b000000000000", "b000000010000", "b000000000000", "b000000110100", "b000000000000", "b000010001011", "b000000000000", "b000101011100", "b000000000000", "b010011111000", "b000000000000", "b101100001000", "b000000000000", "b111010100100", "b000000000000", "b111101110101", "b000000000000", "b111111001100", "b000000000000", "b111111110000", "b000000000000", "b111111111100", "b000000000000", "b000000000000", "b000000000000").map(c => Cat(c(11).U(12.W), c.U).asFixedPoint(11.BP))
 
   val I_delay = Module (new GenericDelayChain(coeffs.length / 2, SInt((params.adcBits + 1).W)))
-  var fir = Module( new GenericFIR(FixedPoint(12.W, 0.BP), FixedPoint(18.W, 11.BP), coeffs) )
+  var fir = Module( new GenericFIR(FixedPoint(24.W, 11.BP), FixedPoint(24.W, 11.BP), coeffs) )
 
   // TODO: might need to add an additional bit in order to make sure that the fixed point value wont be negative
   //io.in.i.data.asFixedPoint(0.BP) // TODO: How does this conversion work? Does this produce an 8 bit FP with the integer component all above the point?
