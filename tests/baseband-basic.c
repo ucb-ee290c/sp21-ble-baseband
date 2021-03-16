e54e19592efc77b785c50c8aa01af092f7520f99
Ryan Lund <ryan.lund@berkeley.edu>
2021-03-15 20:26:29 -0700

    Syntax fixes in baseband C tests

--- tests/baseband-basic.c
+++ tests/baseband-basic.c
@@ -10,8 +10,8 @@ int main(void)
 {
   printf("Pushing a channel index configure\n");
 
-  reg_write32(BASEBAND_ADDITIONAL_DATA, 5)
-  reg_write32(BASEBAND_INSTRUCTION, BASEBAND_INSTRUCTION(BASEBAND_CONFIG, BASEBAND_CONFIG_CHANNEL_INDEX, 0))
+  reg_write32(BASEBAND_ADDITIONAL_DATA, 5);
+  reg_write32(BASEBAND_INST, BASEBAND_INSTRUCTION(BASEBAND_CONFIG, BASEBAND_CONFIG_CHANNEL_INDEX, 0));
 
   printf("Done configuring\n");
 
