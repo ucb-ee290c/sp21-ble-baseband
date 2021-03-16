dcd0a9a17a59d88bb0cf960eeed6d9fa8560fa5d
Griffin Prechter <griffinprechter@MacBook-Pro-3.local>
2021-03-15 22:28:24 -0700

    repurposed AFS for config for image reject image side operation, fixed LO LUT config

--- src/main/scala/baseband/Controller.scala
+++ src/main/scala/baseband/Controller.scala
@@ -304,17 +304,17 @@ class Controller(params: BLEBasebandModemParams, beatBytes: Int) extends Module 
               is (BasebandISA.CONFIG_CHANNEL_INDEX) {
                 constants.channelIndex := io.cmd.bits.additionalData(5, 0)
               }
-              is (BasebandISA.CONFIG_ADDITIONAL_FRAME_SPACE) {
-                constants.additionalFrameSpace := io.cmd.bits.additionalData
+              is (BasebandISA.CONFIG_IMAGE_COMBINE_OP) {
+                constants.imageRejectionCombOp := io.cmd.bits.additionalData(0)
               }
               is (BasebandISA.CONFIG_LO_LUT) { // write an entry into the LUTs for the LO
                 switch (io.cmd.bits.inst.data(6)) { // 7th bit refers to which LUT to write to
                   val addr = io.cmd.bits.inst.data(5, 0) // the 6 bit address in the LUT
                   is (0.U) { // Write to the FSK LUT
-                    constants.LOFSK(addr) := io.cmd.bits.additionalData;
+                    constants.LOFSK(addr) := io.cmd.bits.additionalData(7, 0);
                   }
                   is (1.U) { // Write to the Coarse Tuning LUT
-                    constants.LOCT(addr) := io.cmd.bits.additionalData;
+                    constants.LOCT(addr) := io.cmd.bits.additionalData(7, 0);
                   }
                 }
               }
