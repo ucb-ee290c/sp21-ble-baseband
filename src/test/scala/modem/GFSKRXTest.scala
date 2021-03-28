2dc82ea0a31668a513c8bee4dbd3ee922f17f256
Griffin Prechter <griffinprechter@bwrcr740-8.eecs.berkeley.edu>
2021-03-27 19:52:42 -0700

    test with mixing downconversion almost working

--- src/test/scala/modem/GFSKRXTest.scala
+++ src/test/scala/modem/GFSKRXTest.scala
@@ -24,7 +24,7 @@ class GFSKRXTest extends AnyFlatSpec with ChiselScalatestTester {
   val F_IF = 2 * MHz
   val F_LO = F_RF - F_IF
   val F_IM = F_LO - F_IF
-  val analog_F_sample = (F_LO * 2 + F_IF + (MHz)) * 2
+  val analog_F_sample = (F_LO * 2 + F_IF + 0.25 * MHz) * 2
   val time_interval = 0.0000001
   val symbol_time = 0.000001
   val digital_clock_F = 20 * MHz
@@ -40,19 +40,18 @@ class GFSKRXTest extends AnyFlatSpec with ChiselScalatestTester {
 
   def RFtoIF(in: Seq[Double]): (Seq[Double], Seq[Double]) = {
     val timeSteps = Seq.tabulate[Double]((analog_F_sample * symbol_time * 10).toInt)(_ * (1/(analog_F_sample)))
-    val rf = {t:Double => math.cos(2 * math.Pi * (F_RF + in((t/(symbol_time/20)).floor.toInt)* 0.25 * MHz) * t + math.Pi / 4)}
+    val rf = {t:Double => math.cos(2 * math.Pi * (F_RF + in((t / 5e-8).floor.toInt) * 0.25 * MHz) * t + math.Pi / 4)}
     val I = {t:Double => rf(t) * math.cos(2 * math.Pi * F_LO * t)}
     val Q = {t:Double => rf(t) * math.sin(2 * math.Pi * F_LO * t)}
-    (timeSteps.map{t:Double => in((t/(symbol_time/20)).floor.toInt)* 0.25 * MHz},Seq())
-    //return (analogLowpass(timeSteps.map{I}, analog_F_sample, 10 * MHz).zipWithIndex.collect {case (e,i) if (i % (analog_F_sample / digital_clock_F).toInt) == 0 => e},
-     // analogLowpass(timeSteps.map{Q}, analog_F_sample, 10 * MHz).zipWithIndex.collect {case (e,i) if (i % (analog_F_sample / digital_clock_F).toInt) == 0 => e})
+    return (analogLowpass(timeSteps.map{I}, analog_F_sample, 10 * MHz).zipWithIndex.collect {case (e,i) if (i % (analog_F_sample / digital_clock_F).toInt) == 0 => e},
+      analogLowpass(timeSteps.map{Q}, analog_F_sample, 10 * MHz).zipWithIndex.collect {case (e,i) if (i % (analog_F_sample / digital_clock_F).toInt) == 0 => e})
   }
 
   it should "Elaborate a modem" in {
     test(new HilbertFilter(new BLEBasebandModemParams)).withAnnotations(Seq(TreadleBackendAnnotation, WriteVcdAnnotation)) { c =>
       c.clock.step(10)
       print(RFtoIF(gausFirData)._1)
-      //print(RFtoIF(gausFirData)._2)
+      print(RFtoIF(gausFirData)._2)
     }
   }
 }
